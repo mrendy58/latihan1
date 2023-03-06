366cab5ff989d5fd142295434f82d2f8389ec843
mrendy58 <derendyyy58@gmail.com>
2023-03-06 11:46:54 +0700

    first commit

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello all!!!" << endl;
+    cout << "SAYA MUDA MENDUNIA" << endl;
+
+    return 0;
+}
---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // Begin
+    // Numeric Luas,p,l
+    double Luas, p, l;
+    // display 'masukan panjangnya = '
+    cout << "masuakn panjangnya = ";
+    // Accept p
+    cin >> p;
+    // display 'masukan lebarnya = '
+    cout << "masukan lebarnya = ";
+    // Accept l
+    cin >> l;
+    // compute Luas = p*l
+    Luas = p*l;
+    // display "Luasnya = +Luas"
+    cout << "Luasnya = " << Luas;
+    // end
+}
